4723e1f9992fc0404df3bcf43a9a73542dc77c35
AnsariQamar <ssky5021@gmail.com>
2022-01-27 21:50:48 +0530

    commit new

---
+++ frontend/src/data.js
@@ -0,0 +1,101 @@
+const data={
+    restaurants :[
+        {
+            id:1,
+            name:"Biryani Blues",
+            cuisine:"Biryani, Hyderabadi, Lucknowi, Kebabs,Deserts,Beverages",
+            rating:4.2,
+            timing:28,
+            price:"$400"
+        },
+        {
+            id:2,
+            name:"The Al Lazeez",
+            cuisine:"Mughlai North Indian",
+            rating:4.0,
+            timing:46,
+            price:"$400"
+        },
+        {
+            id:3,
+            name:"For God's Cake",
+            cuisine:"Bakery, Deserts, Beverages, Pizzas, Pastas, Snacks",
+            rating:4.0,
+            timing:46,
+            price:"$200"
+        },
+        {
+            id:4,
+            name:"Pizza Hut",
+            cuisine:"Fast Food, Pizzas",
+            rating:3.8,
+            timing:27,
+            price:"$350"
+        },
+        {
+            id:5,
+            name:"Concept",
+            cuisine:"North indian, Fast Food, Mughlai",
+            rating:4.2,
+            timing:35,
+            price:"$350"
+        },
+        {
+            id:6,
+            name:"Hunger Strike",
+            cuisine:"North Indian, Tandoor",
+            rating:4.0,
+            timing:43,
+            price:"$250"
+        },
+        {
+            id:7,
+            name:"Barbeque Nation",
+            cuisine:"North indian, Mughlai, Seafood, Biryani, Deserts, Kebabs",
+            rating:3.8,
+            timing:29,
+            price:"$600"
+        },
+        {
+            id:8,
+            name:"Hudson Chopsticks",
+            cuisine:"Chinese, Pan-Asian, Street Food, Snacks, Thai",
+            rating:4.3,
+            timing:44,
+            price:"$300"
+        },
+        {
+            id:9,
+            name:"Theobroma",
+            cuisine:"Desserts, Bakery, Beverages",
+            rating:4.4,
+            timing:25,
+            price:"$800"
+        },
+        {
+            id:10,
+            name:"Moolchand Paratha(Moolchand Metro)",
+            cuisine:"North Indian, Punjabi, Snacks, Fast Food",
+            rating:4.3,
+            timing:38,
+            price:"$200"
+        },
+        {
+            id:11,
+            name:"Mad Over Donuts",
+            cuisine:"Desserts,Sweets,Bakery",
+            rating:4.2,
+            timing:28,
+            price:"$400"
+        },
+        {
+            id:12,
+            name:"Delhi Darbar Dhaba",
+            cuisine:"North indian, Chinese",
+            rating:3.7,
+            timing:26,
+            price:"$400"
+        }
+
+    ]
+}

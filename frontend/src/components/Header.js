5a6f72d07c44a4f75a1e0b1f71a54eec4695a377
vikas sharma <vikassharma85109@gmail.com>
2022-01-27 21:18:44 +0530

    new commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return <div>
+     <>
+     <div className='navbar-class'>
+     <nav>Search</nav>
+     <nav>Offers</nav>
+     <nav>Help</nav>
+     <nav>User Name</nav>
+     <nav>Cart</nav>
+     
+     </div>
+     </>
+  </div>;
+}
+
+export default Header;

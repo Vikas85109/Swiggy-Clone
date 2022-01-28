6f07410de44c3bd9b1f71c428d64e3952e5aa51d
vikas sharma <vikassharma85109@gmail.com>
2022-01-28 17:16:31 +0530

    Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Product from "./components/Product";
+import LoginPage from "./components/LoginPage";
 function App() {
   return (
     <>
+     
+      <LoginPage/>
       <Header/>
       <Content></Content> 
       <Product></Product>
-      
 
     </>
   );

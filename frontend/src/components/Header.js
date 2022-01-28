5703652ebc87c813ac22e84c7b3db516806061bf
vikas sharma <vikassharma85109@gmail.com>
2022-01-28 09:46:31 +0530

    update hearder

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React from "react";
+
+import Styles from "./Header.module.css";
 
 function Header() {
-  return <div>
-     <>
-     <div className='navbar-class'>
-     <nav>Search</nav>
-     <nav>Offers</nav>
-     <nav>Help</nav>
-     <nav>User Name</nav>
-     <nav>Cart</nav>
-     
-     
-     </div>
-     </>
-  </div>;
+  return (
+    <>
+    <nav>
+    <img src="./swiggy.png" alt="" />
+    <ul className={Styles.ul}>
+    <li>Search</li>
+    <li>Offers</li>
+    <li>Help</li>
+    <li>Username</li>
+    <li>Cart</li>
+    </ul>
+    </nav>
+
+
+
+
+      {/* <div className={Styles.header}>
+      <div className={Styles.leftheader}>
+          <div className={Styles.left}>
+          <div className={Styles.logo}>
+            <img src="./swiggy.png" alt="" />
+          </div>
+          <div className={Styles.location}>
+            <div> Gurgaon</div>
+            <div className="state">Haryana,india</div>
+          </div>
+          </div>
+        </div>
+       
+        <div className={Styles.rightheader}>
+          
+        </div>
+      </div> */}
+    </>
+  );
 }
 
 export default Header;

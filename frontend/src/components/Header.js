eb1daa5a1b8c141076efca8a8456420140374009
vikas sharma <vikassharma85109@gmail.com>
2022-01-28 13:39:18 +0530

    vikas new commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,37 +5,32 @@ import Styles from "./Header.module.css";
 function Header() {
   return (
     <>
-    <nav>
-    <img src="./swiggy.png" alt="" />
-    <ul className={Styles.ul}>
-    <li>Search</li>
-    <li>Offers</li>
-    <li>Help</li>
-    <li>Username</li>
-    <li>Cart</li>
-    </ul>
-    </nav>
+    <div className={Styles.main_header}>
+    <div className={Styles.header_left}>
+      <div className="logo">
+        <img src="./swiggy.png" alt="" />
+      </div>
+      <div className={Styles.location}>
+       <span className={Styles.underline}> Gurugram</span> Haryana,india
+      </div>
+    </div>
 
-
-
-
-      {/* <div className={Styles.header}>
-      <div className={Styles.leftheader}>
-          <div className={Styles.left}>
-          <div className={Styles.logo}>
-            <img src="./swiggy.png" alt="" />
-          </div>
-          <div className={Styles.location}>
-            <div> Gurgaon</div>
-            <div className="state">Haryana,india</div>
-          </div>
-          </div>
-        </div>
+   <div className={Styles.header_right}>
+     <nav className="navbar">
+       <ul className={Styles.ul}>
        
-        <div className={Styles.rightheader}>
-          
-        </div>
-      </div> */}
+         <li className="">Search</li>
+         
+         
+         <li>Offers</li>
+         <li>Help</li>
+         <li>UserName</li>
+         <li>Cart</li>
+       </ul>
+     </nav>
+   </div>
+
+    </div>
     </>
   );
 }

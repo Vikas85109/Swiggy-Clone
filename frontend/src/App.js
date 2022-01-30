200fda7d7e30ee4794418834f1a6cd49309aa7ff
AnsariQamar <ssky5021@gmail.com>
2022-01-30 19:32:43 +0530

    product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Product from "./components/Product";
 import LoginPage from "./components/LoginPage";
+import ContextWrapper from './context/ContextWrapper'
+import ProductPage from "./components/ProductPage";
 function App() {
   return (
-    <>
-      <Header/>
-      <LoginPage/>
-      <Content></Content> 
-     
-      
+    <Router>
+        {/* // <ContextWrapper> */}
+        <Header/>
+        <Routes>
+          {/* <LoginPage/> */}
+          {/* <Content></Content>  */}
+          <Route path="/" element={<Content/>}/>
+          <Route path="/Product/:id" element={<ProductPage/>}/>
 
-    </>
+        </Routes>
+
+        {/* </ContextWrapper> */}
+      </Router>
   );
   
 }
